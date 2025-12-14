a722cb754edc2d380390d927f8d8343611950401
unknown <korhoff@mail.ru>
2025-12-14 11:40:07 +0400

    deploy

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import './index.css'
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/AdminReactPanel">
         <App />
       </BrowserRouter>
     </Provider>
