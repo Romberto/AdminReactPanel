399726f53f9c59c3595c073e7800af160f0fc8a0
unknown <korhoff@mail.ru>
2025-12-24 12:35:18 +0400

    blog work

--- src/features/blog/pages/BlogCreatePage.tsx
+++ src/features/blog/pages/BlogCreatePage.tsx
@@ -1,7 +1,7 @@
-import { useCreateBlogMutation } from "../../../api/blogsApi"
-import { useNavigate } from "react-router-dom"
-import { BlogForm } from "../components/BlogForm"
-import { BlogFormValues } from "../BlogForm.shemas"
+import { useCreateBlogMutation } from '../../../api/blogsApi'
+import { useNavigate } from 'react-router-dom'
+import { BlogForm } from '../components/BlogForm'
+import { BlogFormValues } from '../BlogForm.shemas'
 
 export default function BlogCreatePage() {
   const [createBlog] = useCreateBlogMutation()
@@ -12,15 +12,13 @@ export default function BlogCreatePage() {
       const res = await createBlog(data).unwrap()
       navigate(`/blogs/${res.slug}`)
     } catch {
-      alert("Create failed")
+      alert('Create failed')
     }
   }
 
   return (
     <div className="p-6 max-w-4xl mx-auto">
-      <h1 className="text-2xl font-semibold mb-6">
-        Создание статьи
-      </h1>
+      <h1 className="text-2xl font-semibold mb-6">Создание статьи</h1>
 
       <BlogForm onSubmit={onSubmit} />
     </div>
